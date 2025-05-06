a0426832eaa65df139743435fc2bd9db21d09b97
Ngf <nirfux@gmail.com>
2025-05-06 10:17:11 +0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -12,16 +12,13 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"Summary generated")
-        elif self.path == '/' or self.path == '/view':
+        elif self.path == '/view':
             self.path = '/docker_summary.html'
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
         else:
             self.send_error(404, "File not found")
 
-# Make sure we are in the correct working directory
 os.chdir("/app")
-
-# Start server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
